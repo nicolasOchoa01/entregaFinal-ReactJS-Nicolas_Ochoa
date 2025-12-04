1840030b0ec16acb6a754d01fe71adaf01e05f01
Nicolas Ochoa <64316712+nicolasOchoa01@users.noreply.github.com>
2025-12-04 12:56:26 -0300

    Change IMGBB API key in uploadImage.js
    Updated IMGBB API key for image upload service.

--- src/services/uploadImage.js
+++ src/services/uploadImage.js
@@ -5,7 +5,7 @@
 // ⚠️ Importante: esta clave queda expuesta en el cliente.
 //     Para prácticas esta ok, pero no es ideal para ambientes reales.
 
-const IMGBB_API_KEY = "5f0228e97400634079f8e098d0c40f8a"; //👈reemplazan por la suya
+const IMGBB_API_KEY = "aa8c1aee9b22d603717618a064b5e677"; //👈reemplazan por la suya
 const ENDPOINT = "https://api.imgbb.com/1/upload";
 
 // Funcion con la que vamos a convertir la imagen (File) a cadena base64
